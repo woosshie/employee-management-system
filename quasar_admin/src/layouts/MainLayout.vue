df174d61f43caf941d41ea8110f71ee078a6a913
woosshie <30732877+woosshie@users.noreply.github.com>
2025-07-14 06:16:56 -0500

    Add main layout component

---
+++ quasar_admin/src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <HeaderMenu :toggleLeftDrawer="toggleLeftDrawer" />
+    </q-header>
+
+    <SidebarMenu />
+
+    <q-page-container class="bg-grey-2">
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import HeaderMenu from 'components/HeaderMenu.vue'
+import SidebarMenu from 'components/SidebarMenu.vue'
+
+import {ref} from 'vue'
+
+export default {
+  components: {
+    HeaderMenu,
+    SidebarMenu
+  },
+
+  setup() {
+    const leftDrawerOpen  = ref(false)
+
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value
+    }
+    return {
+      toggleLeftDrawer,
+      leftDrawerOpen
+    }
+  }
+}
+</script>
